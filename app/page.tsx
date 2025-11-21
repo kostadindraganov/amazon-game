a823b8f3be6442fb315fead49011fe9ffd47f173
Kostadin Draganov <kostadindraganov@gmail.com>
2025-11-21 21:26:15 +0200

    remove text coin

--- app/page.tsx
+++ app/page.tsx
@@ -39,7 +39,7 @@ export default function Home() {
           <div className="flex items-center justify-center gap-2 text-5xl text-casino-gold font-semibold my-3">
             <span className="text-6xl text-white font-bold">{settings.min_points_for_play}</span>
             <img src="/coin.png" alt="Coin" className="w-10 h-10" />
-            <span className="text-2xl text-white">монети</span>
+
           </div>
         </div>
 
